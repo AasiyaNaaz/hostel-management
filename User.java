4ab8492db9913dabb4994fef1b3ac9c5fc2a20c6
Aasiya Naaz <aasiyanaaz2003@gmail.com>
2025-04-15 21:36:29 +0530

    user class

---
+++ User.java
@@ -0,0 +1,164 @@
+package backend;
+
+import java.sql.*;	
+
+public class User {
+
+	public String currentUser;
+	userTypes ut;
+	public enum userTypes {
+		Admin,
+		Student,
+		SuperAdmin
+		}
+	
+	String checkUserType(String username)
+	{
+		Statement st=dbCommands.getStatement();
+		String sqlStr ="select userType from USERS where userName = '" + username + "'";
+		ResultSet resultSet;
+		String userType = "";
+		try {
+			resultSet = st.executeQuery(sqlStr);
+			resultSet.next();
+			userType = resultSet.getString("userType");	
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return userType;
+	}
+	
+	int createUser(String userName,String pwd,userTypes userType,String email)
+	{
+		Statement st=dbCommands.getStatement();
+		String sqlStr ="SELECT * FROM STUDENT WHERE ID = '" + userName + "'";
+		ResultSet resultSet;
+		int count = 0;
+		
+		try {
+			//Check if student exists in student table.
+			resultSet = st.executeQuery(sqlStr);
+			if (!resultSet.next()) 
+			{
+				System.out.println("Student does not exists..!!!");
+				return -1;
+			}
+			
+			//Check if user already exists.
+			String sqlStr1 ="SELECT * FROM USERS WHERE userName = '" + userName + "'";
+			resultSet = st.executeQuery(sqlStr1);
+			if (resultSet.next()) 
+			{
+				System.out.println("user already exists..!!!");
+				return -2;
+			}
+			
+			sqlStr1 = "INSERT INTO USERS (userName,password,userType,emailID) values ('" + userName + "', '" + pwd +"', '" + userType +"','" + email + "')";			
+			count = st.executeUpdate(sqlStr1);
+				//System.out.println(count + " record(s) updated.");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return count;
+		
+	}
+	
+	int forgetPassword(String username, String newPassword)
+	{
+
+		Statement st=dbCommands.getStatement();
+		String sqlStr1 = "UPDATE USERS SET password = '" + newPassword + "' where userName ='" + username + "'";
+
+		int count = 0;
+		try {
+			count = st.executeUpdate(sqlStr1);
+			//System.out.println(count + " record(s) updated.");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return count;
+	}
+	
+	int changePassword(String studentID, String oldPassword, String newPassword)
+	{
+
+		Statement st=dbCommands.getStatement();
+		String sqlStr1 = "UPDATE USERS SET password = '" + newPassword + "' where userName ='" + studentID + "'";
+		String check = "Select password from users where userName = '" + studentID + "'";
+		int count = 0;
+		ResultSet resultSet;
+		try {
+			resultSet = st.executeQuery(check);
+			
+			if (resultSet.next())
+			{
+				if (oldPassword.equals(resultSet.getString("password")))
+				{
+					count = st.executeUpdate(sqlStr1);	
+				} else {
+					return -1;
+				}
+			} else {
+				return -2;
+			}
+			
+			//System.out.println(count + " record(s) updated.");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return count;
+	}
+	
+	int login(String userID,String password)
+	{
+		ResultSet resultSet;
+		
+		try {
+			Statement st=dbCommands.getStatement();
+			String sqlStr ="SELECT password FROM USERS WHERE userName = '" + userID + "'";
+			resultSet = st.executeQuery(sqlStr);
+			if (!resultSet.next()) 
+			{
+				//System.out.println("user does not exists.");
+				return -1;
+			}
+			String str1 = resultSet.getString("password");
+			if (str1.equals(password))
+			{
+				this.currentUser = userID;
+				//System.out.println("LOGIN SUCCESSFULL!");
+			} else {
+				//System.out.println("Wrong password!");
+				return -2;
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return 1;
+		
+	}
+	
+	int logout() 
+	{
+		if(this.currentUser == null)
+		{
+			System.out.println("User isnt logged in!");
+			return -1;
+		}
+		this.currentUser = null;
+		System.out.println("Succesfully logged out.");
+		return 1;
+	}
+
+}
